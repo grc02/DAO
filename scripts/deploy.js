7669423999c2ca84406de1de93a34f92bee8740c
grc02 <georgi.chonkov27@gmail.com>
2022-10-07 20:56:58 +0300

    Script for testing the deployment of the 'Treasury.sol' contract

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,26 +1,23 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const { ethers } = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const [acc1] = await ethers.getSigners();
+  const amount = ethers.utils.parseEther("1");
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+  const Treasury = await ethers.getContractFactory("Treasury");
+  const treasury = await Treasury.deploy(acc1.address, { value: amount });
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  await treasury.deployed();
 
-  await lock.deployed();
+  console.log(`Treasury deployed to ${treasury.address}`);
 
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  const totalFunds = await treasury.totalFunds();
+  const receiver = await treasury.receiver();
+  const isReleased = await treasury.isReleased();
+
+  console.log(parseInt(totalFunds));
+  console.log(receiver);
+  console.log(isReleased);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
