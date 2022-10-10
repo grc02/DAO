9597c16d587caeb19f4077106f7bfa68b8c3bb55
grc02 <georgi.chonkov27@gmail.com>
2022-10-10 22:05:00 +0300

    Added a treasury contract representing the initialization of a treasury

---
+++ contracts/Treasury.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Treasury is Ownable {
+    uint256 public totalFunds;
+    address public receiver;
+    bool public isReleased;
+
+    event treasuryInitiated(address indexed receiver, uint256 value);
+    event fundsReleased(address indexed to, uint256 amount, uint256 timestamp);
+
+    constructor(address _receiver) payable {
+        totalFunds = msg.value;
+        receiver = _receiver;
+        isReleased = false;
+
+        emit treasuryInitiated(receiver, totalFunds);
+    }
+
+    function releaseFunds() public onlyOwner {
+        isReleased = true;
+        payable(receiver).transfer(totalFunds);
+
+        emit fundsReleased(receiver, totalFunds, block.timestamp);
+    }
+}
