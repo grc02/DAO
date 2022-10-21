2e07d4be5123b74fffa830b921aa75e0f54c4c12
grc02 <georgi.chonkov27@gmail.com>
2022-10-21 23:07:34 +0300

    Added functionality for deploying the GovernanceTokenDAO.sol contract

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,11 +1,27 @@
 const { ethers } = require("hardhat");
 
 async function main() {
-  const [acc1] = await ethers.getSigners();
-  const amount = ethers.utils.parseEther("1");
+  const [deployer, executor, proposer, voter1, voter2, voter3, voter4, voter5] =
+    await ethers.getSigners();
+  const amount = ethers.utils.parseEther("50");
+
+  const Token = await ethers.getContractFactory("GovernanceTokenDAO");
+  const token = await Token.deploy();
+
+  await token.connect(deployer).transfer(voter1.address, amount);
+  await token.connect(deployer).transfer(voter2.address, amount);
+  await token.connect(deployer).transfer(voter3.address, amount);
+  await token.connect(deployer).transfer(voter4.address, amount);
+  await token.connect(deployer).transfer(voter5.address, amount);
+
+  const balanceVoter1 = await token.balanceOf(voter1.address);
+  const balanceVoter4 = await token.balanceOf(voter4.address);
+
+  console.log(ethers.utils.formatEther(balanceVoter1));
+  console.log(ethers.utils.formatEther(balanceVoter4));
 
   const Treasury = await ethers.getContractFactory("Treasury");
-  const treasury = await Treasury.deploy(acc1.address, { value: amount });
+  const treasury = await Treasury.deploy(deployer.address, { value: amount });
 
   await treasury.deployed();
 
