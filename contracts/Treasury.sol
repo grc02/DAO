d0658ca837c9a1a188127c63fa5dffc6b5350738
grc02 <georgi.chonkov27@gmail.com>
2022-10-21 13:00:13 +0300

    Added a modifier and a custom error to check if the funds have been already released

--- contracts/Treasury.sol
+++ contracts/Treasury.sol
@@ -8,8 +8,17 @@ contract Treasury is Ownable {
     address public receiver;
     bool public isReleased;
 
+    error alreadyReleased();
+
     event treasuryInitiated(address indexed receiver, uint256 value);
     event fundsReleased(address indexed to, uint256 amount, uint256 timestamp);
+
+    modifier checkReleased() {
+        if (isReleased) {
+            revert alreadyReleased();
+        }
+        _;
+    }
 
     constructor(address _receiver) payable {
         totalFunds = msg.value;
@@ -19,7 +28,7 @@ contract Treasury is Ownable {
         emit treasuryInitiated(receiver, totalFunds);
     }
 
-    function releaseFunds() public onlyOwner {
+    function releaseFunds() public onlyOwner checkReleased {
         isReleased = true;
         payable(receiver).transfer(totalFunds);
 
