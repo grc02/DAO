521a8b37286358e90ce9a1372e3a3f6476408be4
grc02 <georgi.chonkov27@gmail.com>
2022-10-27 18:26:51 +0300

    New const variables added to the helpers-hardhat-config.ts file

--- helper-hardhat-config.ts
+++ helper-hardhat-config.ts
@@ -1,1 +1,5 @@
 export const MIN_DELAY = 60;
+export const QUORUM_PERCENTAGE = 3;
+export const VOTING_PERIOD = 5;
+export const VOTING_DELAY = 1;
+export const ADDRESS_ZERO = "0x0000000000000000000000000000000000000000";
