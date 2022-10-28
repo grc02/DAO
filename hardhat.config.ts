3874c6d7b2e7d661c6fe34c24d6fcb45a4a84db9
grc02 <georgi.chonkov27@gmail.com>
2022-10-28 18:43:56 +0300

    Added a new proposal.json file to keep track of all proposals and updated the dependencies and packages

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -22,6 +22,12 @@ const config: HardhatUserConfig = {
   },
   solidity: {
     version: '0.8.17',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
   },
   namedAccounts: {
     deployer: {
--- helper-hardhat-config.ts
+++ helper-hardhat-config.ts
@@ -4,3 +4,8 @@ export const VOTING_PERIOD = 5;
 export const VOTING_DELAY = 1;
 export const ADDRESS_ZERO = "0x0000000000000000000000000000000000000000";
 export const VALUE = "1000000000000000000";
+export const FUNC_SIGNATURE = "releaseFunds";
+export const RECEIVER_OF_FUNDS = "0x70997970C51812dc3A010C7d01b50e0d17dc79C8";
+
+export const developmentChains = ["hardhat", "localhost"];
+export const proposalFile = "proposals.json";
