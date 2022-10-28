702bc52b62e65aacec33d4c1976c68be2ea41b77
grc02 <georgi.chonkov27@gmail.com>
2022-10-29 00:02:31 +0300

    Modified const vars

--- helper-hardhat-config.ts
+++ helper-hardhat-config.ts
@@ -1,6 +1,6 @@
 export const MIN_DELAY = 60;
 export const QUORUM_PERCENTAGE = 3;
-export const VOTING_PERIOD = 25;
+export const VOTING_PERIOD = 5;
 export const VOTING_DELAY = 1;
 export const ADDRESS_ZERO = "0x0000000000000000000000000000000000000000";
 export const VALUE = "1000000000000000000";
@@ -9,3 +9,4 @@ export const RECEIVER_OF_FUNDS = "0x70997970C51812dc3A010C7d01b50e0d17dc79C8";
 
 export const developmentChains = ["hardhat", "localhost"];
 export const proposalFile = "proposals.json";
+export const PROPOSAL_DESCRIPTION = `Send funds to this address: ${RECEIVER_OF_FUNDS}`;
