ae2a7e909249c34d81c8ba06ead475edc7a6f65e
grc02 <georgi.chonkov27@gmail.com>
2022-10-28 23:43:41 +0300

    Ignoring the proposals.json file

--- helper-hardhat-config.ts
+++ helper-hardhat-config.ts
@@ -1,6 +1,6 @@
 export const MIN_DELAY = 60;
 export const QUORUM_PERCENTAGE = 3;
-export const VOTING_PERIOD = 5;
+export const VOTING_PERIOD = 25;
 export const VOTING_DELAY = 1;
 export const ADDRESS_ZERO = "0x0000000000000000000000000000000000000000";
 export const VALUE = "1000000000000000000";
